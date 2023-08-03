082c074122686d37d93232a0b26913edc901fb10
KmLPro <kamil.bytner@gmail.com>
2023-08-03 22:32:59 +0200

    Migrate queries to Pipelines

--- src/ToDoAsLessCodeAsPossible.BuildingBlocks.Abstractions/Queries/IQueryHandler.cs
+++ src/ToDoAsLessCodeAsPossible.BuildingBlocks.Abstractions/Queries/IQueryHandler.cs
@@ -1,6 +1,6 @@
 namespace ToDoAsLessCodeAsPossible.BuildingBlocks.Abstractions.Queries;
 
-public interface IQueryHandler<in TQuery, TResult> where TQuery: IQuery<TResult>
+public interface IQueryHandler<in TQuery, TResult> where TQuery : IQuery<TResult>
 {
     Task<TResult> HandleAsync(TQuery query, CancellationToken cancellationToken);
 }
--- src/ToDoAsLessCodeAsPossible.BuildingBlocks.Infrastructure/Queries/Pipeline/Validation/ValidationQueryPipelineBehavior.cs
+++ src/ToDoAsLessCodeAsPossible.BuildingBlocks.Infrastructure/Queries/Pipeline/Validation/ValidationQueryPipelineBehavior.cs
@@ -5,19 +5,23 @@ using ToDoAsLessCodeAsPossible.BuildingBlocks.Infrastructure.Queries.Exceptions;
 
 namespace ToDoAsLessCodeAsPossible.BuildingBlocks.Infrastructure.Queries.Pipeline.Validation;
 
-public class ValidationQueryPipelineBehavior: IQueryPipelineBehavior
+public class ValidationQueryPipelineBehavior<TQuery, TResult> : IQueryHandler<TQuery, TResult>
+    where TQuery : IQuery<TResult>
 {
     private const string QUERY_VALIDATE_STRUCT_METHOD_NAME = "ValidateStruct";
     private const string QUERY_VALIDATE_USECASE_RULES_METHOD_NAME = "ValidateUseCaseRules";
 
     private readonly IServiceScopeFactory _serviceFactory;
+    private readonly IQueryHandler<TQuery, TResult> _queryHandler;
 
-    public ValidationQueryPipelineBehavior(IServiceScopeFactory serviceFactory)
+    public ValidationQueryPipelineBehavior(IServiceScopeFactory serviceFactory,
+        IQueryHandler<TQuery, TResult> queryHandler)
     {
         _serviceFactory = serviceFactory;
+        _queryHandler = queryHandler;
     }
-    
-    public async Task<TResult> HandleAsync<TQuery, TResult>(TQuery query, CancellationToken cancellationToken, QueryHandlerDelegate<TResult> next) where TQuery : IQuery<TResult>
+
+    public async Task<TResult> HandleAsync(TQuery query, CancellationToken cancellationToken)
     {
         using var scope = _serviceFactory.CreateScope();
         var provider = scope.ServiceProvider;
@@ -25,58 +29,62 @@ public class ValidationQueryPipelineBehavior: IQueryPipelineBehavior
         var queryType = query.GetType();
         var queryName = queryType.Name;
 
-        var structValidationResult = ValidateStruct(provider,queryType, typeof(TResult), queryName, query);
+        var structValidationResult = ValidateStruct(provider, queryType, typeof(TResult), queryName, query);
 
         if (structValidationResult.Any())
         {
             throw new InvalidQueryStructException(structValidationResult);
         }
 
-        var useCaseRulesValidationResult = await ValidateUseCaseRules(provider,queryType, typeof(TResult), queryName, query, cancellationToken);
+        var useCaseRulesValidationResult =
+            await ValidateUseCaseRules(provider, queryType, typeof(TResult), queryName, query, cancellationToken);
 
         if (useCaseRulesValidationResult.Any())
         {
             throw new QueryRulesBrokenException(useCaseRulesValidationResult);
         }
-        
-        return await next().ConfigureAwait(false);
+
+        return await _queryHandler.HandleAsync(query, cancellationToken).ConfigureAwait(false);
     }
 
-    private List<string> ValidateStruct<TQuery>(IServiceProvider provider, Type queryType, Type resultType, string queryName, TQuery query)
+    private List<string> ValidateStruct<TQuery>(IServiceProvider provider, Type queryType, Type resultType,
+        string queryName, TQuery query)
     {
         var validator = ValidatorGetter.GetStructValidator(provider, queryType, resultType, queryName);
         if (validator == null)
         {
             return new List<string>();
         }
-        
-        MethodInfo methodInfo = MethodInfoGetter.GetByName(validator,QUERY_VALIDATE_STRUCT_METHOD_NAME);
-        
+
+        MethodInfo methodInfo = MethodInfoGetter.GetByName(validator, QUERY_VALIDATE_STRUCT_METHOD_NAME);
+
         if (methodInfo == null)
         {
             throw new MethodNotFoundInGenericTypeException(queryName, QUERY_VALIDATE_STRUCT_METHOD_NAME);
         }
-        
+
         return MethodExecutor.Invoke<List<string>>(methodInfo, validator, (dynamic)query);
     }
-    
-    private async Task<List<string>> ValidateUseCaseRules<TQuery>(IServiceProvider provider, Type queryType, Type resultType, string queryName, TQuery query, CancellationToken cancellationToken)
+
+    private async Task<List<string>> ValidateUseCaseRules<TQuery>(IServiceProvider provider, Type queryType,
+        Type resultType, string queryName, TQuery query, CancellationToken cancellationToken)
     {
         var validator = ValidatorGetter.GetUseCaseRulesValidator(provider, queryType, resultType, queryName);
         if (validator == null)
         {
             return new List<string>();
         }
-        
-        MethodInfo methodInfo = MethodInfoGetter.GetByName(validator,QUERY_VALIDATE_USECASE_RULES_METHOD_NAME);
+
+        MethodInfo methodInfo = MethodInfoGetter.GetByName(validator, QUERY_VALIDATE_USECASE_RULES_METHOD_NAME);
 
         if (methodInfo == null)
         {
             throw new MethodNotFoundInGenericTypeException(queryName, QUERY_VALIDATE_USECASE_RULES_METHOD_NAME);
         }
 
-       
-        List<string> result = await MethodExecutor.InvokeAsync<List<string>>(methodInfo, validator, (dynamic)query, cancellationToken);
+
+        List<string> result =
+            await MethodExecutor.InvokeAsync<List<string>>(methodInfo, validator, (dynamic)query, cancellationToken);
 
         return result;
     }
